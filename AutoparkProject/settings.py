f8e49785ec65237e882a5829b1b2e6e23f0c6be4
lenkoran26 <karnel26@gmail.com>
2024-02-22 19:12:56 +0300

    ..

--- AutoparkProject/settings.py
+++ AutoparkProject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',    
+    'django.contrib.staticfiles',
+    'sorl.thumbnail',    
     'drivers',
     'users',
     'employees',
@@ -122,6 +123,7 @@ DATE_INPUT_FORMATS = ["%d-%m-%Y", "%d.%m.%Y"]
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
